bf2ff53b2831da854bdfda15afdf3b93f725c2e8
Juan Andres Vivas <juan.vivas16@gmail.com>
2016-11-29 23:59:57 -0400

    Se actualizo todos los esquemas de la bd....

--- src/etc/init_db.sql
+++ src/etc/init_db.sql
@@ -9,19 +9,6 @@ DROP DATABASE IF EXISTS `testgym`;
 CREATE DATABASE `testgym` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `testgym`;
 
-DROP TABLE IF EXISTS `cafetin`;
-CREATE TABLE `cafetin` (
-  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(20) NOT NULL,
-  `precio` float NOT NULL,
-  `cantidad` int(10) unsigned NOT NULL,
-  `id_proveedor` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_producto`),
-  KEY `id_proveedor` (`id_proveedor`),
-  CONSTRAINT `cafetin_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`rif`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
 DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE `cliente` (
   `ci_cliente` varchar(10) NOT NULL,
@@ -37,25 +24,54 @@ CREATE TABLE `egreso` (
   `nombre` varchar(20) NOT NULL,
   `fecha` date NOT NULL,
   `total` float NOT NULL,
-  PRIMARY KEY (`id_egreso`)
+  `id_usuario` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_egreso`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `egreso_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`ci_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `ingresos`;
 CREATE TABLE `ingresos` (
-  `id_pago` int(11) NOT NULL AUTO_INCREMENT,
+  `id_ingreso` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` date NOT NULL,
   `descripcion` varchar(20) NOT NULL,
-  `iva` float unsigned NOT NULL,
+  `iva` float unsigned NOT NULL DEFAULT '0.12',
   `sub_total` float unsigned NOT NULL,
   `total` float unsigned NOT NULL,
   `ci_cliente` varchar(10) NOT NULL,
-  `id_cafetin` int(11) NOT NULL,
-  PRIMARY KEY (`id_pago`),
+  `id_producto` int(11) NOT NULL,
+  `id_usuario` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_ingreso`),
   KEY `ci_cliente` (`ci_cliente`),
-  KEY `id_cafetin` (`id_cafetin`),
-  CONSTRAINT `ingresos_ibfk_1` FOREIGN KEY (`ci_cliente`) REFERENCES `cliente` (`ci_cliente`) ON DELETE NO ACTION,
-  CONSTRAINT `ingresos_ibfk_2` FOREIGN KEY (`id_cafetin`) REFERENCES `cafetin` (`id_producto`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `id_producto` (`id_producto`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `ingresos_ibfk_1` FOREIGN KEY (`ci_cliente`) REFERENCES `cliente` (`ci_cliente`),
+  CONSTRAINT `ingresos_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
+  CONSTRAINT `ingresos_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`ci_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+
+DELIMITER ;;
+
+CREATE TRIGGER `sub_total_bi` BEFORE INSERT ON `ingresos` FOR EACH ROW
+  BEGIN
+    IF NEW.sub_total > 0 THEN
+      SET @sub_total = -(NEW.sub_total);
+    END IF;
+  END;;
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS `ingreso_producto`;
+CREATE TABLE `ingreso_producto` (
+  `id_ingreso` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) unsigned NOT NULL DEFAULT '1',
+  KEY `id_ingreso` (`id_ingreso`),
+  KEY `id_producto` (`id_producto`),
+  CONSTRAINT `ingreso_producto_ibfk_1` FOREIGN KEY (`id_ingreso`) REFERENCES `ingresos` (`id_ingreso`),
+  CONSTRAINT `ingreso_producto_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -66,21 +82,11 @@ CREATE TABLE `inventario_equipo` (
   `modelo` varchar(20) NOT NULL,
   `fecha_compra` date NOT NULL,
   `costo` float NOT NULL,
-  `estado` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_equipo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-DROP TABLE IF EXISTS `membresia`;
-CREATE TABLE `membresia` (
-  `ci_cliente` varchar(10) NOT NULL,
-  `ci_usuario` varchar(10) NOT NULL,
-  `tipo_membresia` varchar(10) NOT NULL,
-  `fecha_vencimiento` date NOT NULL,
-  PRIMARY KEY (`ci_cliente`,`ci_usuario`),
-  KEY `ci_usuario` (`ci_usuario`),
-  CONSTRAINT `membresia_ibfk_1` FOREIGN KEY (`ci_cliente`) REFERENCES `cliente` (`ci_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `membresia_ibfk_2` FOREIGN KEY (`ci_usuario`) REFERENCES `usuario` (`ci_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
+  `estado` enum('Activo','Inactivo') NOT NULL,
+  `id_usuario` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_equipo`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `inventario_equipo_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`ci_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -107,6 +113,27 @@ CREATE TABLE `personal` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `producto`;
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `precio` float unsigned NOT NULL,
+  `cantidad_disponible` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `provee`;
+CREATE TABLE `provee` (
+  `rif_proveedor` varchar(10) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  KEY `rif_proveedor` (`rif_proveedor`),
+  KEY `id_producto` (`id_producto`),
+  CONSTRAINT `provee_ibfk_1` FOREIGN KEY (`rif_proveedor`) REFERENCES `proveedor` (`rif`),
+  CONSTRAINT `provee_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 DROP TABLE IF EXISTS `proveedor`;
 CREATE TABLE `proveedor` (
   `rif` varchar(10) NOT NULL,
@@ -114,7 +141,23 @@ CREATE TABLE `proveedor` (
   `telefono` varchar(12) NOT NULL,
   `direccion` varchar(100) NOT NULL,
   `desc_producto` varchar(20) NOT NULL,
-  PRIMARY KEY (`rif`)
+  `id_usuario` varchar(10) NOT NULL,
+  PRIMARY KEY (`rif`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `proveedor_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`ci_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `servicio`;
+CREATE TABLE `servicio` (
+  `tipo` enum('Diario','Quincenal','Mensual') NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `id_ingreso` int(11) NOT NULL,
+  `ci_cliente` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_ingreso`,`ci_cliente`),
+  KEY `ci_cliente` (`ci_cliente`),
+  CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`id_ingreso`) REFERENCES `ingresos` (`id_ingreso`),
+  CONSTRAINT `servicio_ibfk_2` FOREIGN KEY (`ci_cliente`) REFERENCES `cliente` (`ci_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -129,4 +172,4 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2016-11-23 04:14:24
+-- 2016-11-30 03:58:36

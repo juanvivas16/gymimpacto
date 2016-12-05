ef219fe8698ff92668ea982676771800c00c8bd0
Juan Andres Vivas <juan.vivas16@gmail.com>
2016-12-05 15:33:36 -0400

    entrega.odt actualizado tipo de dato nombre

--- src/data_model/Person.java
+++ src/data_model/Person.java
@@ -2,6 +2,7 @@ package data_model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Date;
 
 /**
  * Created by juancho on 05/12/16.
@@ -10,7 +11,24 @@ import javax.persistence.Id;
 @Entity
 public class Person
 {
-  @Id private String _ci;
+  @Id private String ci;
+  private String name;
+  private String last_name;
+  private Date birth_date;
+  private String gender;
+  private String dir;
+  private String phone;
   
+  public Person( ) {}
   
+  public Person(String ci, String name, String last_name, Date birth_date, String gender, String dir, String phone)
+  {
+    this.ci = ci;
+    this.name = name;
+    this.last_name = last_name;
+    this.birth_date = birth_date;
+    this.gender = gender;
+    this.dir = dir;
+    this.phone = phone;
+  }
 }
